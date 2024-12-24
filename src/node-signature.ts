c1ec7285fd68ac91fd9c12086f66844ed7a2e575
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-12-24 11:45:18 -0500

    Cleaned up the signature but I need tests.

--- src/node-signature.ts
+++ src/node-signature.ts
@@ -29,156 +29,82 @@ It should be noted that vsCode resolves this be resolving destructuring to an an
 I think having a method that better at differenciating such things.
 */
 
-import { getDocPath, getFName, getFullName, getName, getTypeNode, isPrimitive } from "./node-tools"
-import { Node, Type, TypedNode, TypeOperatorTypeNode } from "ts-morph"
+import { getDocPath, getFullName, getName, getTypeNode, isPrimitive } from "./node-tools";
+import { ArrowFunction, FunctionDeclaration, FunctionExpression, FunctionTypeNode, MethodDeclaration, MethodSignature, Node, Type, TypeOperatorTypeNode } from "ts-morph";
 import { bySyntax } from "./SyntaxKindDelegator";
 import SK, { SKindMap } from "./SyntaxKindDelegator.types";
 import { $href, $kind, $literal, $name, $type } from "./decorators";
-import { gray, green, yellow } from "console-log-colors";
+import { gray, yellow } from "console-log-colors";
 import { Nodely } from "./types";
 import TS from "./TS";
 import { typelit } from "./constants";
-function escape(str:string) {
-	const htmlEntities = {
-	  '<': '&lt;',
-	  '>': '&gt;',
-	  '&': '&amp;',
-	  '"': '&quot;',
-	  "'": '&apos;',
-	  '/': '&#47;',
-	  '\\': '&#92;',
-	  '{': '&#123;',
-	  '}': '&#125;',
-	  '`': '&#96;'
-	};
-	
-	return str.replace(/[<>&"'/\\{}`]/g, char => htmlEntities[char as keyof typeof htmlEntities]);
-}
+import { escape } from "./utils";
 
+/**
+ * With typenodes not alway being provided this method acts as a point where I can change the logic if getting a typenode fails.
+ * @param node 
+ * @returns 
+ */
 const fromTypeNode = (node: Node): string => {
 	const tn = getTypeNode(node);
 	return tn ? sig(tn)
 	: getSignatureFromType(node) ?? ''
 }
+
+const objLiteral = () => $literal(typelit);
+
+const genTypes = (nodes: Node[], pre: string ='<', post: string = '>') => nodes.map(sig).join(', ').wrap(pre, post);
+
+const isAsync = (node: Node) => {
+	if(!('isAsync' in node) || typeof node.isAsync !== 'function') return false;
+	return node.isAsync();
+}
+const isGenerator = (node: Node) => {
+	if(!('isGenerator' in node) || typeof node.isGenerator !== 'function') return false;
+	return node.isGenerator();
+}
+const fnSignature = (node: FunctionDeclaration | FunctionExpression | MethodDeclaration | FunctionTypeNode | MethodSignature | ArrowFunction) => `${isAsync(node) ? 'async ':''}${isGenerator(node) ? '*':''}${genTypes(node.getTypeParameters())}(${genTypes(node.getParameters(), '', '')}) =&gt; ${sig(node.getReturnTypeNode()) || $literal('void')}`;
+
 const typingMap: SKindMap<string> = {
-	[SK.TypeAliasDeclaration]: (node, df)=>{
-		const params = node.getTypeParameters()?.map(p=>sig(p)).join(', ').wrap('<','>') ?? '';
-		const typeNode = fromTypeNode(node)
-		if(!typeNode) return df(node)
-		return params+': ' + typeNode;
-	},
-	[SK.LiteralType]: node=>$literal(node.getText()),
-	[SK.TupleType]: node=>{
-		return `[${node.getElements().map(e=>sig(e)).join(', ')}]`
-	},
-	[SK.UnionType]: node=>node.getTypeNodes().map(e=>sig(e)).join(' | '),
-	[SK.IntersectionType]: node=>node.getTypeNodes().map(e=>sig(e)).join(' & '),
-	[SK.NamedTupleMember]: node=>`${$name(node.getName())}: ${sig(node.getTypeNode()!)}`,
-	[SK.ArrayType]: node=>`${sig(node.getElementTypeNode())}[]`,
-	[SK.TypeReference]: node=>{ //this is a tricky one
-		const typeName = node.getTypeName();
-		const args = node.getTypeArguments();
-		//corner cases
-		if(typeName.getText() === "Array"){
-			return sig(args[0])+"[]";
-		}
-
-		return sig(typeName) + (args.length ? args.map(a=>sig(a)).join(', ').wrap('<','>'):'')
-	},
-	[SK.Identifier]: node=>{
-		const def = node.getDefinitionNodes()[0]; //I guess its possible to have multiple definitions but I havent thought of a use case where I would have reference to all definitions in one location (extensions would have a link back to immediate source automatically)
-		if(!def) return '';$type(node.getText()); //no link
-		const href = getDocPath(def)
-		if(!href) return $type(node.getText()); //link outside scope
-		return $href(node.getText(), href);
-	},
-	[SK.TypeParameter]: node => {
-		const extension = node.getConstraint()
-		return $name(node.getName()) + (extension ? ' extends ' + sig(extension):'');
-	},
-	[SK.TypeLiteral]: ()=>$type('&lcub;...&rcub;'),
+	//functional declarations
+	[SK.FunctionDeclaration]: fnSignature,
+	[SK.FunctionExpression]: fnSignature,
+	[SK.MethodDeclaration]: fnSignature,
+	[SK.FunctionType]: fnSignature,
+	[SK.MethodSignature]: fnSignature,
+	[SK.ArrowFunction]: fnSignature,
 	[SK.PropertySignature]: fromTypeNode,
-	[SK.MethodSignature]: node=>{
-		return `(${node.getParameters().map(p=>sig(p)).join(', ')}): ${sig(node.getReturnTypeNode())}`;
-	},
-	[SK.Parameter]: node=>{
-		const typeNode = fromTypeNode(node);
-		const initializer = sig(node.getInitializer());
-		return `${$name((node.isRestParameter() ? '...':'')+sig(node.getNameNode()))}: ${typeNode}${initializer ? ' = '+initializer:''}`;
-	},
-	[SK.ArrayLiteralExpression]: node=>'[]',
-	[SK.FunctionType]: node=>{
-		return `(${node.getParameters().map(p=>sig(p)).join(', ')}) =&gt; ${sig(node.getReturnTypeNode()) || $literal('void')}`;
-	},
-	[SK.MethodDeclaration]: node=>{
-		return `(${node.getParameters().map(p=>sig(p)).join(', ')}) =&gt; ${sig(node.getReturnTypeNode()) || $literal('void')}`;
-	},
-	[SK.ArrowFunction]: node=>{
-		return `(${node.getParameters().map(p=>sig(p)).join(', ')}) =&gt; ${sig(node.getReturnTypeNode()) || $literal('void')}`;
-	},
+	[SK.BindingElement]: fromTypeNode,
+	[SK.PropertyAccessExpression]: node => `${sig(node.getNameNode())}`,
+	[SK.UnionType]: node=>node.getTypeNodes().map(sig).join(' | '),
+	[SK.LiteralType]: node=>$literal(node.getText()),
+	[SK.IntersectionType]: node=>node.getTypeNodes().map(sig).join(' & '),
+	[SK.NamedTupleMember]: node=>`${$name(node.getName())}: ${fromTypeNode(node)}`,
+	[SK.ArrayType]: node=>`${sig(node.getElementTypeNode())}[]`,
+	[SK.ArrayLiteralExpression]: ()=>'[]',
+	[SK.TupleType]: node=> genTypes(node.getElements(), '[',']'),
 	[SK.ParenthesizedType]: node=>`(${fromTypeNode(node)})`,
-	[SK.ClassDeclaration]: node=>{
-		const extensions = sig(node.getExtends());
-		const implementions = node.getImplements().map(i=>sig(i)).join(', ');
-		return `${extensions ? ' extends '+ extensions:''}${implementions ? ' implements ' + implementions:''}`
-	},
-	[SK.ExpressionWithTypeArguments]: node=>{
-		const args = node.getTypeArguments().map(a=>sig(a)).join(', ');
-		return `${sig(node.getExpression())}${args.wrap('<','>')}`
-	},
-	[SK.InterfaceDeclaration]: node=>$literal(typelit),
 	[SK.Constructor]: node=>`${$type("new")} (${node.getParameters().map(p=>sig(p)).join(', ')})=>${$type(getName(node.getParent()))}`,
-	[SK.GetAccessor]: node=> {
-		const rtn = node.getReturnTypeNode();
-		return rtn ? sig(rtn)
-		: getSignatureFromType(node) ?? ''
-	},
 	[SK.SetAccessor]: node=> node.getParameters().map(p=>sig(p)).join(', '),
-	[SK.ConditionalType]: node => {
-		return `${sig(node.getCheckType())} extends ${sig(node.getExtendsType())} ? ${sig(node.getTrueType())}<br/>: ${sig(node.getFalseType())}`;
-	},
-	[SK.VariableDeclaration]: node => {
-		const tn = fromTypeNode(node);
-		if(tn) return tn;
-		const init = node.getInitializer();
-		return sig(init);
-	},
-
-	[SK.FunctionExpression]: node => {
-		return `(${node.getParameters().map(p=>sig(p)).join(', ')}) =&gt; ${sig(node.getReturnTypeNode()) || $literal('void')}`;
-	},
-	[SK.ObjectLiteralExpression]: node => $literal('&lcub;...&rcub;'),
-	[SK.ObjectBindingPattern]: node => {
-		return `&lcub; ... &rcub;`
-	},
-	[SK.BindingElement]: node => {
-		return sig(node.getNameNode())
-	},
-	[SK.ArrayBindingPattern]: node => {
-		return `[${node.getElements().map(n=>sig(n)).join(', ')}]`
-	},
-	[SK.QualifiedName]: node => {
-		return `${sig(node.getLeft())}.${sig(node.getRight())}`;
-	},
-	[SK.TypePredicate]: node => {
-		return `${sig(node.getParameterNameNode())} ${node.hasAssertsModifier() ? sig(node.getAssertsModifier()):'is'} ${sig(node.getTypeNode())}`;
-	},
-	[SK.TypeOperator]: node => {
-		return `${$kind(getOperator(node))} ${sig(node.getTypeNode())}`;
-	},
+	[SK.ConditionalType]: node => `${sig(node.getCheckType())} extends ${sig(node.getExtendsType())} ? ${sig(node.getTrueType())}<br/>: ${sig(node.getFalseType())}`,
+	[SK.ExpressionWithTypeArguments]: node => `${sig(node.getExpression())}${node.getTypeArguments().map(a=>sig(a)).join(', ').wrap('<','>')}`,
+	[SK.RestType]: node => `...${fromTypeNode(node)}`,
+	[SK.ArrayBindingPattern]: node => genTypes(node.getElements(), '[',']'),
+	[SK.QualifiedName]: node => `${sig(node.getLeft())}.${sig(node.getRight())}`,
+	[SK.TypePredicate]: node => `${sig(node.getParameterNameNode())} ${node.hasAssertsModifier() ? sig(node.getAssertsModifier()):'is'} ${sig(node.getTypeNode())}`,
+	[SK.TypeOperator]: node => `${$kind(getOperator(node))} ${fromTypeNode}`,
 	[SK.BinaryExpression]: node => `${sig(node.getLeft())} ${sig(node.getOperatorToken())} ${sig(node.getRight())}`,
-
-	[SK.PropertyAccessExpression]: node => {
-		return `${sig(node.getNameNode())}`;
-	},
-	[SK.CallExpression]: node => {
-		return $type(escape(node.getReturnType().getText())) // todo decode types
-	},
-	[SK.RestType]: node => `...${sig(node.getTypeNode())}`,
+	[SK.CallExpression]: node => $type(escape(node.getReturnType().getText())),
+	
 	[SK.IndexedAccessType]: node => `${sig(node.getObjectTypeNode())}[${node.getIndexTypeNode()}]`,
-	[SK.FunctionDeclaration]: node=>{
-		return `(${node.getParameters().map(p=>sig(p)).join(', ')}) =&gt; ${sig(node.getReturnTypeNode()) || $literal('void')}`;
-	},
+
+	//object literat expressions or declarations or types
+	[SK.ObjectLiteralExpression]: objLiteral,
+	[SK.ObjectBindingPattern]: objLiteral,
+	[SK.TypeLiteral]: objLiteral,
+	[SK.InterfaceDeclaration]: objLiteral, //this isnt that simple
+
+	
 
 	//tokens
 	[SK.AsteriskToken]: () => '*',
@@ -196,7 +122,54 @@ const typingMap: SKindMap<string> = {
 	[SK.LessThanToken]: ()=>'&lt;',
 	[SK.LessThanEqualsToken]: ()=>'&lt;=',
 	[SK.GreaterThanToken]: ()=>'&gt;',
-	[SK.GreaterThanEqualsToken]: ()=>'&gt;='
+	[SK.GreaterThanEqualsToken]: ()=>'&gt;=',
+
+	[SK.TypeAliasDeclaration]: (node, df)=>`${genTypes(node.getTypeParameters())}: ${fromTypeNode}`,
+	[SK.TypeReference]: node=>{ 
+		const typeName = node.getTypeName();
+		const args = node.getTypeArguments();
+		//corner cases
+		if(typeName.getText() === "Array"){
+			return sig(args[0])+"[]";
+		}
+		return sig(typeName) + args.map(sig).join(', ').wrap('<', '>')
+	},
+	[SK.Identifier]: node=>{
+		const def = node.getDefinitionNodes()[0]; //I guess its possible to have multiple definitions but I havent thought of a use case where I would have reference to all definitions in one location (extensions would have a link back to immediate source automatically)
+		if(!def) return $type(node.getText()); //no link
+		const href = getDocPath(def)
+		if(!href) return $type(node.getText()); //link outside scope
+		return $href(node.getText(), href);
+	},
+	[SK.TypeParameter]: node => {
+		const extension = node.getConstraint()
+		const modifiers = node.getModifiers()
+		return `${modifiers.map(sig).join(' ')}${modifiers.length ? ' ':''}${$name(node.getName())}${(extension ? ' extends ' + sig(extension):'')}`;
+	},
+	[SK.Parameter]: node=>{
+		const typeNode = fromTypeNode(node);
+		const initializer = sig(node.getInitializer());
+		return `${$name((node.isRestParameter() ? '...':'')+sig(node.getNameNode()))}: ${typeNode}${initializer ? ' = '+initializer:''}`;
+	},
+	[SK.ClassDeclaration]: node=>{
+		const extensions = sig(node.getExtends());
+		const implementions = node.getImplements().map(i=>sig(i)).join(', ');
+		return `${extensions ? ' extends '+ extensions:''}${implementions ? ' implements ' + implementions:''}`
+	},
+	[SK.GetAccessor]: node=> {
+		const rtn = node.getReturnTypeNode();
+		return rtn ? sig(rtn)
+		: getSignatureFromType(node) ?? ''
+	},
+
+	[SK.VariableDeclaration]: node => {
+		const tn = fromTypeNode(node);
+		if(tn) return tn;
+		const init = node.getInitializer();
+		return sig(init);
+	},
+
+	
 }
 
 const getOperator = (node: TypeOperatorTypeNode) =>{
@@ -206,9 +179,11 @@ const getOperator = (node: TypeOperatorTypeNode) =>{
 		case SK.UniqueKeyword: return 'unique';
 	}
 }
+
 const Ignores = new Set([
 	SK.MultiLineCommentTrivia
-])
+]);
+
 const defSig = (n: Nodely)=>{
 	if(!n || Ignores.has(n.getKind())) return '';
 	if(isPrimitive(n)) return $type(n.getText());
